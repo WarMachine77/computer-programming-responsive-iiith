fe6f5ed5bf3c7fd3d6224d83dc5976730765f65f
Siddharth Jain <jainsiddharth7747@gmail.com>
2020-07-03 19:35:45 +0530

    fixed bugs related to simulation

--- src/lab/exp4/simulation/1-D/array.js
+++ src/lab/exp4/simulation/1-D/array.js
@@ -83,10 +83,10 @@ window.view = {
 	},
 	takeInputFromRadioBox: function() {
 		var element = document.getElementsByName('radio_group')
-		if ( element[0].checked )
-			this.generateRandomNumbers()
-		else if (element[1].checked)
+		if ( element[1].checked )
 			this.getUserInput()
+		else if (element[0].checked) 
+			this.generateRandomNumbers()
 	},
 	createBoxes: function() {
 		for ( i = 0 ; i < this.numbers.length ; i++ ) {
